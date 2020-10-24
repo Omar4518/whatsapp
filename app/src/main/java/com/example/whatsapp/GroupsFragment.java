43691cf549a03f504c6cd32a8b419f56fcde216c
Omar4518 <omarm4518@gmail.com>
2020-10-24 21:24:10 +0200

    login and register finished and settings activity done

--- app/src/main/java/com/example/whatsapp/GroupsFragment.java
+++ app/src/main/java/com/example/whatsapp/GroupsFragment.java
@@ -1,12 +1,29 @@
 package com.example.whatsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +31,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class GroupsFragment extends Fragment {
+    private View groupFragmentView;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> listOfGroups = new ArrayList<>();
+    private DatabaseReference GroupRef;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -57,8 +79,57 @@ public class GroupsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+                             Bundle savedInstanceState)
+    {
+
+        groupFragmentView =  inflater.inflate(R.layout.fragment_groups, container, false);
+        GroupRef= FirebaseDatabase.getInstance().getReference().child("Groups");
+        InitializeFields();
+        RetrieveAndDisplayGroups();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                String currentGroupName = parent.getItemAtPosition(position).toString();
+                Intent groupChatIntent = new Intent(getContext(),GroupChatActivity.class);
+                groupChatIntent.putExtra("groupName",currentGroupName);
+                startActivity(groupChatIntent);
+
+            }
+        });
+        return groupFragmentView;
+    }
+
+
+
+    private void InitializeFields()
+    {
+        listView =  (ListView) groupFragmentView.findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter<String>(Objects.requireNonNull(getContext()),android.R.layout.simple_list_item_1,listOfGroups);
+        listView.setAdapter(arrayAdapter);
+    }
+    private void RetrieveAndDisplayGroups()
+    {
+        GroupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                Set<String> set = new HashSet<>();
+                Iterator iterator = snapshot.getChildren().iterator();
+
+                while (iterator.hasNext())
+                {
+                    set.add(((DataSnapshot)iterator.next()).getKey());
+                }
+                listOfGroups.clear();
+                listOfGroups.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
