d9654313a80ea92abab37106a70d66d4b55aefb7
Omar4518 <omarm4518@gmail.com>
2020-11-03 19:02:34 +0200

    display chat requests on request fragment

--- app/src/main/java/com/example/whatsapp/TabsAccessorAdabtor.java
+++ app/src/main/java/com/example/whatsapp/TabsAccessorAdabtor.java
@@ -25,6 +25,9 @@ public class TabsAccessorAdabtor extends FragmentPagerAdapter {
             case 2:
                 ContactsFragment contactsFragment = new ContactsFragment();
                 return contactsFragment;
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
             default:
                 return null;
 
@@ -34,7 +37,7 @@ public class TabsAccessorAdabtor extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 3;
+        return 4;
     }
 
     @Nullable
@@ -48,6 +51,9 @@ public class TabsAccessorAdabtor extends FragmentPagerAdapter {
                 return "Groups";
             case 2:
                 return "Contacts";
+            case 3:
+                return "Requests";
+
 
 
             default:
